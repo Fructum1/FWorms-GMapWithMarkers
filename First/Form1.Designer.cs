a56cf1440ac079a05f06c6456d3b4c4ac6b5c269
Fructum1 <79308915+Fructum1@users.noreply.github.com>
2022-04-21 16:23:18 +0700

    Initial commit

---
+++ First/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace First
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemory = 5;
+            this.gmap.Location = new System.Drawing.Point(-3, -2);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 2;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(1485, 1045);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 0D;
+            this.gmap.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gMapControl1_OnMarkerClick);
+            this.gmap.Load += new System.EventHandler(this.gMapControl1_Load);
+            this.gmap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gmap_MouseClick);
+            //this.gmap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseDown);
+            //this.gmap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseMove);
+            //this.gmap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseUp);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(1488, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(404, 228);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "Чтобы начать перемещение маркера нажмите ЛКМ, чтобы сохранить и поставить в новое" +
+    " место - ПКМ\"";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.gmap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ First/Form1.cs
@@ -0,0 +1,101 @@
+﻿using GMap.NET.WindowsForms;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace First
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private bool clicked = false;
+        private readonly string connectionString = "Server=DESKTOP-N3K4LAO\\SSQE;Database=GeographicСoordinates;Trusted_Connection=True;";
+        GMapOverlay markers = new GMapOverlay("markers");
+        private GMapMarker marker;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void  gMapControl1_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; //выбор подгрузки карты – онлайн или из ресурсов
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; //какой провайдер карт используется (в нашем случае гугл) 
+            gmap.MinZoom = 2; //минимальный зум
+            gmap.MaxZoom = 16; //максимальный зум
+            gmap.Zoom = 14; // какой используется зум при открытии
+            gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; // как приближает (просто в центр карты или по положению мыши)
+            gmap.CanDragMap = true; // перетаскивание карты мышью
+            gmap.DragButton = MouseButtons.Middle; // какой кнопкой осуществляется перетаскивание
+            gmap.ShowCenter = false; //показывать или скрывать красный крестик в центре
+            gmap.ShowTileGridLines = false; //показывать или скрывать тайлы
+            gmap.Position = new GMap.NET.PointLatLng(55.59907310721567, 37.28551369055172);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM machine_coordinates", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows) // если есть данные
+                {
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        double lat = reader.GetDouble(1);
+                        double lng = reader.GetDouble(2);
+                        string info = reader.GetString(3);
+
+
+                        GMapMarker marker =
+                            new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                            new GMap.NET.PointLatLng(lat, lng),
+                                GMap.NET.WindowsForms.Markers.GMarkerGoogleType.blue_pushpin);
+                        marker.ToolTipText = info;
+
+                        markers.Markers.Add(marker);
+                    }
+                }
+            }
+            gmap.Overlays.Add(markers);
+        }
+
+        private void gMapControl1_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                marker = item;
+                clicked = true;
+            }
+            else
+            {
+                marker = null;
+            }
+        }
+
+        private void gmap_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (clicked && marker != null && e.Button == MouseButtons.Right)
+            {
+                marker.Position = new GMap.NET.PointLatLng(
+                gmap.FromLocalToLatLng(
+                    e.Location.X, e.Location.Y).Lat,
+                gmap.FromLocalToLatLng(
+                    e.Location.X, e.Location.Y).Lng);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    int id = markers.Markers.TakeWhile(m => m != marker).Count() + 1;
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(
+                        $"UPDATE machine_coordinates " +
+                        $"SET latitude ={marker.Position.Lat.ToString().Replace(",", ".")}, longitude ={marker.Position.Lng.ToString().Replace(",", ".")} " +
+                        $"WHERE id = {id}", connection);
+                    command.ExecuteNonQuery(); 
+                }
+            }
+            clicked = false;
+            marker = null;
+        }
+    }
+}
