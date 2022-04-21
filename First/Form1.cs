ca47db67f3480010d0fa2de9125024f668aec88c
Fructum1 <79308915+Fructum1@users.noreply.github.com>
2022-04-21 16:32:11 +0700

    some code changes

--- First/Form1.cs
+++ First/Form1.cs
@@ -8,94 +8,94 @@ namespace First
 {
     public partial class Form : System.Windows.Forms.Form
     {
-        private bool clicked = false;
-        private readonly string connectionString = "Server=DESKTOP-N3K4LAO\\SSQE;Database=GeographicСoordinates;Trusted_Connection=True;";
-        GMapOverlay markers = new GMapOverlay("markers");
-        private GMapMarker marker;
+        private bool _clicked = false;
+        private readonly string _connectionString = "Server=DESKTOP-N3K4LAO\\SSQE;Database=GeographicСoordinates;Trusted_Connection=True;";
+        GMapOverlay _markers = new GMapOverlay("markers");
+        private GMapMarker _marker;
+
 
         public Form()
         {
             InitializeComponent();
         }
 
-
-
         private void  gMapControl1_Load(object sender, EventArgs e)
         {
-            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; //выбор подгрузки карты – онлайн или из ресурсов
-            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; //какой провайдер карт используется (в нашем случае гугл) 
-            gmap.MinZoom = 2; //минимальный зум
-            gmap.MaxZoom = 16; //максимальный зум
-            gmap.Zoom = 14; // какой используется зум при открытии
-            gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; // как приближает (просто в центр карты или по положению мыши)
-            gmap.CanDragMap = true; // перетаскивание карты мышью
-            gmap.DragButton = MouseButtons.Middle; // какой кнопкой осуществляется перетаскивание
-            gmap.ShowCenter = false; //показывать или скрывать красный крестик в центре
-            gmap.ShowTileGridLines = false; //показывать или скрывать тайлы
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            gmap.MinZoom = 2;
+            gmap.MaxZoom = 16;
+            gmap.Zoom = 14; 
+            gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; 
+            gmap.CanDragMap = true; 
+            gmap.DragButton = MouseButtons.Middle; 
+            gmap.ShowCenter = false; 
+            gmap.ShowTileGridLines = false; 
             gmap.Position = new GMap.NET.PointLatLng(55.59907310721567, 37.28551369055172);
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand("SELECT * FROM machine_coordinates", connection);
                 SqlDataReader reader = command.ExecuteReader();
-                if (reader.HasRows) // если есть данные
+                if (reader.HasRows)
                 {
-                    while (reader.Read()) // построчно считываем данные
+                    while (reader.Read()) 
                     {
                         double lat = reader.GetDouble(1);
                         double lng = reader.GetDouble(2);
                         string info = reader.GetString(3);
 
-
                         GMapMarker marker =
                             new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
                             new GMap.NET.PointLatLng(lat, lng),
                                 GMap.NET.WindowsForms.Markers.GMarkerGoogleType.blue_pushpin);
-                        marker.ToolTipText = info;
 
-                        markers.Markers.Add(marker);
+                        marker.ToolTipText = info;
+                        _markers.Markers.Add(marker);
                     }
                 }
             }
-            gmap.Overlays.Add(markers);
+            gmap.Overlays.Add(_markers);
         }
 
         private void gMapControl1_OnMarkerClick(GMapMarker item, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                marker = item;
-                clicked = true;
+                _marker = item;
+                _clicked = true;
             }
             else
             {
-                marker = null;
+                _marker = null;
             }
         }
 
         private void gmap_MouseClick(object sender, MouseEventArgs e)
         {
-            if (clicked && marker != null && e.Button == MouseButtons.Right)
+            if (_clicked && _marker != null && e.Button == MouseButtons.Right)
             {
-                marker.Position = new GMap.NET.PointLatLng(
+                _marker.Position = new GMap.NET.PointLatLng(
                 gmap.FromLocalToLatLng(
                     e.Location.X, e.Location.Y).Lat,
                 gmap.FromLocalToLatLng(
                     e.Location.X, e.Location.Y).Lng);
-                using (SqlConnection connection = new SqlConnection(connectionString))
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    int id = markers.Markers.TakeWhile(m => m != marker).Count() + 1;
+                    int id = _markers.Markers.TakeWhile(m => m != _marker).Count() + 1;
                     connection.Open();
                     SqlCommand command = new SqlCommand(
                         $"UPDATE machine_coordinates " +
-                        $"SET latitude ={marker.Position.Lat.ToString().Replace(",", ".")}, longitude ={marker.Position.Lng.ToString().Replace(",", ".")} " +
+                        $"SET latitude ={_marker.Position.Lat.ToString().Replace(",", ".")}, longitude ={_marker.Position.Lng.ToString().Replace(",", ".")} " +
                         $"WHERE id = {id}", connection);
                     command.ExecuteNonQuery(); 
                 }
             }
-            clicked = false;
-            marker = null;
+
+            _clicked = false;
+            _marker = null;
         }
     }
 }
